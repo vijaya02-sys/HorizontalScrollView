462edfb19755dec4dca24610a3e53fb057a35b6b
vijaya <vilajaxmyai@gmail.com>
2023-04-19 11:18:30 +0530

    git commit activity_main.xml

--- app/src/main/java/com/geekyhub/horizontalscrollview/MainActivity.java
+++ app/src/main/java/com/geekyhub/horizontalscrollview/MainActivity.java
@@ -3,12 +3,25 @@ package com.geekyhub.horizontalscrollview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "There is a click on this button", Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
